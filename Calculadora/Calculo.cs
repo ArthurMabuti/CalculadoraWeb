23d072ccb8a2342173a35912422ae3fd6c973a17
arthu <arthu@TUTS-PC>
2024-11-26 23:12:47 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Calculo.cs
@@ -0,0 +1,41 @@
+﻿namespace Calculadora;
+internal class Calculo(double total, double novoNumero, string operacao)
+{
+    public double Total { get; set; } = total;
+    public double NovoNumero
+    {
+        get => novoNumero;
+        set
+        {
+            if (PrimeiraOperacao == true && Total == 0)
+            {
+                Total = value;
+            }
+            else
+            {
+                novoNumero = value;
+            }
+        }
+    }
+    public string? Operacao { get; set; } = operacao;
+    public bool PrimeiraOperacao { get; set; } = true;
+
+    public void Resultado()
+    {
+        var operacaoFunc = ObterFuncaoDeCalculo(Operacao!);
+
+        Total = operacaoFunc(Total, NovoNumero);
+    }
+
+    private static Func<double, double, double> ObterFuncaoDeCalculo(string operacao)
+    {
+        return operacao switch
+        {
+            "+" => (total, valor) => total + valor,
+            "-" => (total, valor) => total - valor,
+            "*" => (total, valor) => total * valor,
+            "/" => (total, valor) => valor != 0 ? total / valor : throw new DivideByZeroException("Divisão por zero não permitida"),
+            _ => throw new ArgumentException("Operação inválida")
+        };
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,52 @@
+﻿using Calculadora;
+using System.Globalization;
+
+Calculo calculo = new(0, 0, null!);
+
+while (true)
+{
+    if (calculo.PrimeiraOperacao == true)
+    {
+        ExecutarPrimeiraOperacao(calculo);
+    }
+    while (calculo.PrimeiraOperacao == false)
+    {
+        SolicitarOperacao(calculo);
+        if (calculo.Operacao!.ToUpper().Equals("C"))
+        {
+            ZerarResultado(calculo);
+            break;
+        }
+        SolicitarNumero(calculo);
+        DemonstrarResultados(calculo);
+    }
+}
+
+static void ExecutarPrimeiraOperacao(Calculo calculo)
+{
+    Console.WriteLine("Executando Primeira Operação");
+    SolicitarNumero(calculo);
+    calculo.PrimeiraOperacao = false;
+}
+static void SolicitarOperacao(Calculo calculo)
+{
+    Console.WriteLine("Escreva a operação a ser feita ( + ; - ; * ; /; %, C (Zerar) )");
+    calculo.Operacao = Console.ReadLine()!;
+}
+static void SolicitarNumero(Calculo calculo)
+{
+    Console.WriteLine("Escreva um número");
+    calculo.NovoNumero = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+}
+static void DemonstrarResultados(Calculo calculo)
+{
+    calculo.Resultado();
+    Console.WriteLine($"Total: {calculo.Total}");
+}
+
+static void ZerarResultado(Calculo calculo)
+{
+    calculo.Total = 0;
+    calculo.PrimeiraOperacao = true;
+    Console.WriteLine("Resultados Zerados");
+}
